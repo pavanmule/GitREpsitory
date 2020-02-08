1096932ea655b34ee7e525e76d0212df0e0d91a1
Puneet <Puneet@Puneet-PC>
2020-02-08 08:05:42 +0530

    Firts Commit

---
+++ src/test/TSET1.java
@@ -0,0 +1,16 @@
+package test;
+
+public class TSET1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String hungry="yes";
+if	(hungry=="yes")
+	System.out.println("eat something");
+else
+{
+	System.out.println("Do Work");
+}
+	}
+
+}
